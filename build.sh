a055c8c1e5daf50a9a8e2f2c7616160fe3a343e8
noone <noone>
2025-08-13 11:37:52 +1000

    update build flags

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export CGO_ENABLED=0
 export GOARCH=arm
 export GOARM=6
-go build -o meshproxy-go-arm -tags withui
+go build -a -ldflags "-s -w" -trimpath -o meshproxy-go-arm -tags withui
 upx meshproxy-go-arm
